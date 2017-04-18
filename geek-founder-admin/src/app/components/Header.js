93e05ce031fd2438fce3dfc83511b7649a8e3462
Diego Oliveira <oliveira.bsi@gmail.com>
2017-04-18 15:38:35 -0300

    chroe: Clean up unnecessaries files on admin skeleton

--- geek-founder-admin/src/app/components/Header.js
+++ geek-founder-admin/src/app/components/Header.js
@@ -1,20 +1,7 @@
 class HeaderController {
-  /** @ngInject */
-  constructor(todoService) {
-    this.todoService = todoService;
-  }
-
-  handleSave(text) {
-    if (text.length !== 0) {
-      this.todos = this.todoService.addTodo(text, this.todos);
-    }
-  }
 }
 
 export const Header = {
   template: require('./Header.html'),
-  controller: HeaderController,
-  bindings: {
-    todos: '='
-  }
+  controller: HeaderController
 };
--- geek-founder-admin/src/app/containers/App.js
+++ geek-founder-admin/src/app/containers/App.js
@@ -1,12 +1,4 @@
-import {SHOW_ALL} from '../constants/TodoFilters';
-import {initialTodo} from '../todos/todos';
-
-class AppController {
-  constructor() {
-    this.todos = [initialTodo];
-    this.filter = SHOW_ALL;
-  }
-}
+class AppController { }
 
 export const App = {
   template: require('./App.html'),
